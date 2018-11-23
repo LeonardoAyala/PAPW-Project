b34924254580623585b3e1186fcbf5794dfa9a67
DESKTOP-TQIF9RM\doble <ayala.santiago.leonardo@gmail.com>
2018-11-22 20:53:27 -0600

    a
    a

--- src/main/java/com/example/CategoriaDAO.java
+++ src/main/java/com/example/CategoriaDAO.java
@@ -1,6 +1,5 @@
 package com.example; 
 import java.util.List; 
-import javax.sql.DataSource; 
 import java.sql.Connection;
  
 public interface CategoriaDAO {    
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -18,14 +18,12 @@ package com.example;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.jdbc.core.JdbcTemplate; 
 
 import javax.sql.DataSource;
 import java.sql.Connection;
